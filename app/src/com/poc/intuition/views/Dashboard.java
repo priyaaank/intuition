4c76688afc8afbcb58dcafbf9eb6265cdd649dfc
Priyank <priyaaank@gmail.com>
2014-05-04 13:25:03 +0530

    Attaching a spending overview fragment

--- app/src/com/poc/intuition/views/Dashboard.java
+++ app/src/com/poc/intuition/views/Dashboard.java
@@ -1,25 +1,42 @@
 package com.poc.intuition.views;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.poc.intuition.R;
 import com.poc.intuition.experiments.DashboardFragment;
+import com.poc.intuition.service.IListener;
+import com.poc.intuition.service.UserStatisticsService;
+import com.poc.intuition.service.response.UserStatisticsResponse;
 
-public class Dashboard extends FragmentActivity {
+public class Dashboard extends FragmentActivity implements IListener<UserStatisticsResponse> {
 
     private SlidingMenu slidingMenu;
+    private UserStatisticsService userStatisticsService;
+    private UserStatisticsResponse userStats;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.navigation_drawer);
         slidingMenu = (SlidingMenu) findViewById(R.id.slidingmenulayout);
+        lookupUserStats();
         updateActionBar();
         attachDefaultFragment();
         attachMenuNavigationEvents();
+    }
+
+    public UserStatisticsResponse getUserStatistics() {
+        return userStats;
+    }
+
+    private void lookupUserStats() {
+        userStatisticsService = UserStatisticsService.singleInstance(getApplicationContext());
+        userStatisticsService.registerListener(this);
+        userStatisticsService.findUserStatsForLastMonths(10);
     }
 
     private void updateActionBar() {
@@ -51,6 +68,28 @@ public class Dashboard extends FragmentActivity {
 
     private void attachMenuNavigationEvents() {
         findViewById(R.id.transaction_history_link).setOnClickListener(navigateToTransactionHistory());
+        findViewById(R.id.historic_spending_overview_link).setOnClickListener(navigateToSpendingOverview());
+        findViewById(R.id.dashboard_link).setOnClickListener(navigateToDashboard());
+    }
+
+    private View.OnClickListener navigateToDashboard() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+          attachDefaultFragment();
+            }
+        };
+    }
+
+    private View.OnClickListener navigateToSpendingOverview() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                slidingMenu.showContent();
+                HistoricSpendingFragment historicSpendingFragment = new HistoricSpendingFragment();
+                attachFragmentWithTagToContentView(historicSpendingFragment, "HistoricSpendingOverview");
+            }
+        };
     }
 
     private View.OnClickListener navigateToTransactionHistory() {
@@ -59,26 +98,32 @@ public class Dashboard extends FragmentActivity {
             public void onClick(View v) {
                 slidingMenu.showContent();
                 TransactionHistoryFragment transactionHistoryFragment = new TransactionHistoryFragment();
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.main_container, transactionHistoryFragment, "TransactionHistory")
-                        .setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right, android.R.anim.slide_in_left, android.R.anim.slide_out_right)
-                        .commit();
+                attachFragmentWithTagToContentView(transactionHistoryFragment, "TransactionHistory");
             }
         };
     }
 
     private void attachDefaultFragment() {
+        slidingMenu.showContent();
         DashboardFragment dashboardFragment = new DashboardFragment();
+        attachFragmentWithTagToContentView(dashboardFragment, "Dashboard");
+    }
+
+    private void attachFragmentWithTagToContentView(Fragment fragment, String tag) {
         getSupportFragmentManager()
                 .beginTransaction()
-                .add(R.id.main_container, dashboardFragment, "Dashboard")
-                .setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right, android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+                .replace(R.id.main_container, fragment, tag)
+                .addToBackStack(null)
                 .commit();
     }
 
     public void updateSelectedTransactionsWithCategory(String categoryName) {
         TransactionHistoryFragment transactionHistory = (TransactionHistoryFragment) getSupportFragmentManager().findFragmentByTag("TransactionHistory");
         transactionHistory.updateSelectedTransactionsWithCategory(categoryName);
+    }
+
+    @Override
+    public void serviceResponse(UserStatisticsResponse response) {
+        this.userStats = response;
     }
 }
---
+++ app/src/com/poc/intuition/views/HistoricSpendingFragment.java
@@ -0,0 +1,18 @@
+package com.poc.intuition.views;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.poc.intuition.R;
+
+public class HistoricSpendingFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View inflatedView = inflater.inflate(R.layout.historic_spending, container, false);
+        return inflatedView;
+    }
+
+}
